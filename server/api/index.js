248cd1d5e4908c43440dc10a453b014eb4511543
Judith Lao <laojudith2014@gmail.com>
2019-07-30 15:44:41 -0400

    associations and simple user routes

--- server/api/index.js
+++ server/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/users', require('./users'))
+// router.use('/orders', require('./orders'))
+// router.use('/products', require('./products'))
 
 router.use((req, res, next) => {
-  const error = new Error('Not Found')
+  const error = new Error('API route not found')
   error.status = 404
   next(error)
 })
--- server/api/users.js
+++ server/api/users.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const {User} = require('../db/models')
+const {User, Order} = require('../db/models')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
@@ -11,6 +11,48 @@ router.get('/', async (req, res, next) => {
       attributes: ['id', 'email']
     })
     res.json(users)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      attributes: ['name', 'email', 'defaultShipping', 'defaultBilling'],
+      where: {
+        id: req.params.id
+      }
+    })
+    res.json(user)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:id/history', async (req, res, next) => {
+  try {
+    const user = await Order.findAll({
+      where: {
+        userId: req.params.id,
+        fulfilled: true
+      }
+    })
+    res.json(user)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:id/cart', async (req, res, next) => {
+  try {
+    const user = await Order.findAll({
+      where: {
+        userId: req.params.id,
+        fulfilled: false
+      }
+    })
+    res.json(user)
   } catch (err) {
     next(err)
   }
