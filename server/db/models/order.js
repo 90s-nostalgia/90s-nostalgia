984e3b95c1ef9ea8d1e9ea3b5e82096386da6bfc
Judith Lao <laojudith2014@gmail.com>
2019-07-31 11:04:58 -0400

    barry helped with database things

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -3,13 +3,6 @@ const db = require('../db')
 
 //link to user, one user has many orders, a one to many relationship establishes a userId column
 const Order = db.define('order', {
-  items: {
-    type: Sequelize.ARRAY(Sequelize.INTEGER),
-    allowNull: false,
-    validate: {
-      notEmpty: true
-    }
-  },
   orderShipping: {
     type: Sequelize.STRING
   },
