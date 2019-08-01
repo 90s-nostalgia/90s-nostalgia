d1e9fef8175cc58fb484c1b44632ec25a786a475
Ani Kerr <ani.kerr@gmail.com>
2019-07-31 20:10:29 -0400

    add to cart - pre-code-review

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,22 +1,36 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {getSingleProduct} from '../store/product'
+import {addToOrder} from '../store/order'
 import {Link} from 'react-router-dom'
 
 export class SingleProduct extends Component {
+  constructor() {
+    super()
+    this.handleClick = this.handleClick.bind(this)
+  }
+
   componentDidMount() {
     const productId = this.props.match.params.productId
     this.props.getSingleProduct(productId)
     console.log(productId)
   }
 
+  handleClick(event) {
+    const productId = this.props.match.params.productId
+    event.preventDefault()
+    this.props.addToOrder(productId)
+  }
+
   render() {
-    console.log(this.props.singleProduct)
     return (
       <div>
         {this.props.singleProduct ? (
           <img src={this.props.singleProduct.imageUrl} />
         ) : null}
+        <button type="button" onClick={this.handleClick}>
+          Add to cart
+        </button>
       </div>
     )
   }
@@ -24,14 +38,16 @@ export class SingleProduct extends Component {
 
 const mapStateToProps = state => {
   return {
-    singleProduct: state.product.singleProduct
+    singleProduct: state.product.singleProduct,
+    orders: state.order.orders
   }
 }
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   const productId = ownProps.match.params.productId
   return {
-    getSingleProduct: () => dispatch(getSingleProduct(productId))
+    getSingleProduct: () => dispatch(getSingleProduct(productId)),
+    addToOrder: () => dispatch(addToOrder(productId))
   }
 }
 
