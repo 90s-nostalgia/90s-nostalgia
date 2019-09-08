5c48acf274122780ee1810441dbf2d90881bfd96
dan.chin <djchinia@gmail.com>
2019-09-08 17:36:27 -0400

    windows compatability

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const isDev = process.env.NODE_ENV === 'development'
 
 module.exports = {
   mode: isDev ? 'development' : 'production',
+  // added
   entry: [
     '@babel/polyfill', // enables async-await
     './client/index.js'
